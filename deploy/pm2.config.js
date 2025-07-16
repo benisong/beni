522d55050163060a6458d06e86a6539e59bd528f
benisong <beni3355229@outlook.com>
2025-07-16 12:30:42 +0800

    Update pm2.config.js

--- deploy/pm2.config.js
+++ deploy/pm2.config.js
@@ -1,1 +1,18 @@
-
+module.exports = {
+  apps : [
+    {
+      name   : "sillytavern-backend",
+      cwd    : "./backend",
+      script : "app.js",
+      watch  : false,
+      env: { "PORT": 3001 }
+    },
+    {
+      name   : "sillytavern-frontend",
+      cwd    : "./frontend",
+      script : "node_modules/.bin/serve",
+      args   : "-s build -l 3000",
+      watch  : false
+    }
+  ]
+}
